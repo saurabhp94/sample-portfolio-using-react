b1e30087d91a0d5ded97d5c7561f15d3528d976a
saurabhp94 <166574015+saurabhp94@users.noreply.github.com>
2024-06-15 00:37:37 -0400

    learning tweaks

--- src/components/containers/DefaultContainers.jsx
+++ src/components/containers/DefaultContainers.jsx
@@ -3,37 +3,24 @@ import "./DefaultContainers.css";
 import { Button } from "react-bootstrap"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-
-
-
-
-
 const DefaultContainers = (props) => {
   const { sectionType } = props;
   const bullets = sectionType && sectionType.bodyText ? sectionType.bodyText.split(/\r\n|\r|\n/) : [];
   const [showMore, setShowMore] = useState(false);
-  
-  
-
   const toggleShowMore = () => {
     setShowMore(!showMore);
   };
-
-  
- 
   const fullBullets = () => {
-    return(
+    return (
       bullets.map((Text, index) => {
         return (
           <ul key={index}>
-          <li>{Text}</li>
+            <li>{Text}</li>
           </ul>
         );
-        
       })
     )
   }
-
   const twoBullets = () => {
     return (
       <ul>
@@ -42,41 +29,34 @@ const DefaultContainers = (props) => {
       </ul>
     );
   }
-
   const regularText = () => {
     return (
       <p>{bullets}</p>
     );
   }
-
   return (
     <div className="container">
-      
       {sectionType?.image &&
-      <img className="image" 
-      src={sectionType?.image} 
-      // onClick={handleShow}
-      alt="ProjectImage" />}
-      
+        <img className="image"
+          src={sectionType?.image}
+          // onClick={handleShow}
+          alt="ProjectImage" />}
       <h3>{sectionType.title}</h3>
-      
-      {sectionType.bodyText &&  (
-          bullets.length > 2 ? 
-           (<div className="button">
+      {sectionType.bodyText && (
+        bullets.length > 2 ?
+          (<div className="button">
             {showMore ? fullBullets() : twoBullets()}
-          <Button variant="primary" onClick={toggleShowMore}>
-            {showMore ? "Show Less" : "Show More"}
-          </Button>
-           </div>
-           ) : (
+            <Button variant="primary" onClick={toggleShowMore}>
+              {showMore ? "Show Less" : "Show More"}
+            </Button>
+          </div>
+          ) : (
             <div className="regularText">
               {regularText()}
-              
             </div>
-           )
+          )
       )
       }
-
       {sectionType.links && (
         <div className="links">
           {sectionType?.links?.map((link, index) => (
@@ -86,15 +66,14 @@ const DefaultContainers = (props) => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FontAwesomeIcon 
-              style={{marginRight:"5px"}}
-              icon={faGithub} />
+              <FontAwesomeIcon
+                style={{ marginRight: "5px" }}
+                icon={faGithub} />
               {link.text}
             </a>
           ))}
         </div>
       )}
-
       <div className="tags-container">
         {sectionType?.tags?.map((tag, index) => (
           <span key={index} className="badge">
@@ -102,19 +81,17 @@ const DefaultContainers = (props) => {
           </span>
         ))}
       </div>
-      
       <div className="footerUrl">
-      {sectionType.credentialUrl &&
-         <a
-          href={sectionType?.credentialUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Verify Credential
-        </a>}
+        {sectionType.credentialUrl &&
+          <a
+            href={sectionType?.credentialUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Verify Credential
+          </a>}
       </div>
     </div>
   );
 };
-
 export default DefaultContainers;
