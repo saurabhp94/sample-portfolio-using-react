03227c10934b110b7eb5dbcd13e34fbfa6f8ea5a
saurabhp94 <166574015+saurabhp94@users.noreply.github.com>
2024-06-14 21:11:43 -0400

    minor trials

--- src/theme/ThemeContext.jsx
+++ src/theme/ThemeContext.jsx
@@ -13,14 +13,14 @@ export const ThemeProvider = ({ children }) => {
             setTheme(prefersColorScheme.matches ? 'dark' : 'light'); // Respect system preference initially 
         }; window.addEventListener('storage', handleSystemThemeChange); // Listen for theme changes in localStorage 
         if (theme === 'dark') {
-            document.documentElement.style.setProperty('--background-color', 'none');
+            document.documentElement.style.setProperty('--background-color', 'linear-gradient(to bottom, #f5f7fa, #c3cfe2);');
             document.documentElement.style.setProperty('--text-color', 'white');
-            document.documentElement.style.setProperty('--background-image', 'url("https://wallpapercave.com/wp/wp5502431.jpg")');
+            // document.documentElement.style.setProperty('--background-image', 'url("https://wallpapercave.com/wp/wp5502431.jpg")');
         }
         else {
-            document.documentElement.style.setProperty('--background-color', 'white');
+            document.documentElement.style.setProperty('--background-color', 'linear-gradient(to bottom, #212129, #4c5265);');
             document.documentElement.style.setProperty('--text-color', 'black');
-            document.documentElement.style.setProperty('--background-image', 'url("https://images.pexels.com/photos/7130560/pexels-photo-7130560.jpeg?cs=srgb&dl=pexels-codioful-7130560.jpg&fm=jpg")');
+            // document.documentElement.style.setProperty('--background-image', 'url("https://images.pexels.com/photos/7130560/pexels-photo-7130560.jpeg?cs=srgb&dl=pexels-codioful-7130560.jpg&fm=jpg")');
         }
 
         return () => {
