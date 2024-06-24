72a6cfcec418d5ea72029b14eb669efb9d871969
saurabhp94 <166574015+saurabhp94@users.noreply.github.com>
2024-06-24 00:15:34 -0400

    hover animation

--- src/Cursor.jsx
+++ src/Cursor.jsx
@@ -51,6 +51,7 @@ export default function App() {
           '[onmouseup]',
           '[ontouchstart]',
           '[ontouchend]',
+          '[switch]',
           {
             target: '.custom',
             options: {
